87fcb7a3f6715c11d6f40c984fd41b83f854719e
Александр <red.hoodie25@gmail.com>
2025-03-20 23:29:05 +0300

    add server info command to fetch and display server details

--- main.go
+++ main.go
@@ -28,6 +28,47 @@ func init() {
 	}
 }
 
+// Функция для получения информации о сервере
+func serverInfo(s *discordgo.Session, channelID string, guildID string) {
+	guild, err := s.Guild(guildID)
+	if err != nil {
+		s.ChannelMessageSend(channelID, "Ошибка получения информации о сервере: "+err.Error())
+		return
+	}
+
+	members, _ := s.GuildMembers(guildID, "", 1000)
+	channels, _ := s.GuildChannels(guildID)
+
+	var textChannels, voiceChannels, categoryChannels int
+	for _, channel := range channels {
+		switch channel.Type {
+		case discordgo.ChannelTypeGuildText:
+			textChannels++
+		case discordgo.ChannelTypeGuildVoice:
+			voiceChannels++
+		case discordgo.ChannelTypeGuildCategory:
+			categoryChannels++
+		}
+	}
+
+	createdAt, _ := discordgo.SnowflakeTimestamp(guild.ID)
+
+	info := fmt.Sprintf("**Информация о сервере**\n"+
+		"Название: %s\n"+
+		"ID: %s\n"+
+		"Владелец: <@%s>\n"+
+		"Создан: %s\n"+
+		"Участников: %d\n"+
+		"Каналов: %d (Текстовых: %d, Голосовых: %d, Категорий: %d)\n"+
+		"Ролей: %d\n"+
+		"Уровень проверки: %d\n",
+		guild.Name, guild.ID, guild.OwnerID, createdAt.Format("02.01.2006 15:04:05"),
+		len(members), len(channels), textChannels, voiceChannels, categoryChannels,
+		len(guild.Roles), guild.VerificationLevel)
+
+	s.ChannelMessageSend(channelID, info)
+}
+
 func main() {
 	sess, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -42,6 +83,11 @@ func main() {
 
 		if m.Content == "привет" {
 			s.ChannelMessageSend(m.ChannelID, "мир!")
+		}
+
+		// Обработка команды !serverinfo
+		if m.Content == prefix+"serverinfo" {
+			serverInfo(s, m.ChannelID, m.GuildID)
 		}
 	})
 
